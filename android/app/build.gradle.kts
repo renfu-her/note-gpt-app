8784a96559652dd16df69d68820a571f000fdf83
Patrick Her <renfu.her@gmail.com>
2025-04-23 17:20:46 +0800

    更新應用程式的命名空間與包識別碼，將其從 com.example.notegpt 變更為 com.notecloud.app。調整版本號至 1.0.1，並新增釋出簽名配置以支持應用程式的發佈。

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.notegpt"
+    namespace = "com.notecloud.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,20 +21,29 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.notegpt"
+        applicationId = "com.notecloud.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = 1
+        versionName = "1.0.1"
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file("release.keystore") // 你的 keystore 檔案
+            storePassword = "hezrid5"
+            keyAlias = "note-cloud"
+            keyPassword = "hezrid5"
+        }
     }
 
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
--- android/app/src/main/kotlin/com/example/notegpt/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/notegpt/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.notegpt
+package com.notecloud.app
 
 import io.flutter.embedding.android.FlutterActivity
 
